8d4612a74f1a23e9f1fd5ee0abc8adf64cf08194
Bogagree <bogatireshka@gmail.com>
2022-10-09 21:07:11 +0300

    refactor: Projects.tsx

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import style from './Projects.module.css';
 import styleContainer from '../common/styles/Container.module.css';
-import Project from "./project/Project";
+import Project from './project/Project';
 import {Title} from '../common/Components/Title/Title';
+import allthaiHerbs from '../assets/img/projectImg.jpg'
 
-declare module '*.jpg';
+const myProjects = [
+    {
+        title: 'Learning cards',
+        description: 'Lorem ipsum dolor sit amet.',
+        link: 'https://allthaiherbs.com/ru',
+        linkTitle: 'allthaiherbs.com',
+        img: allthaiHerbs,
+        technologies: ['html5', 'css3', 'JavaScript', 'React', 'Redux']
+    },
+    {
+        title: 'Social Network',
+        description: 'Lorem ipsum dolor sit amet.',
+        link: 'https://bogagree.github.io/samurai-way-main/#/profile',
+        linkTitle: 'allthaiherbs.com',
+        img: allthaiHerbs,
+        technologies: ['html5', 'css3', 'JavaScript', 'React', 'Redux']
+    },
+    {
+        title: 'Allthaiherbs.com',
+        description: 'internet shop with Thai traditional medicine and natural cosmetic',
+        link: 'https://allthaiherbs.com/ru',
+        linkTitle: 'Allthaiherbs.com',
+        img: allthaiHerbs,
+        technologies: ['html5', 'css3', 'JavaScript', 'React', 'Redux']
+    },
+]
 
 
-const Projects = () => {
+const Projects: React.FC = () => {
+
     return (
         <div className={style.projectsBlock}>
 
@@ -16,21 +43,16 @@ const Projects = () => {
                 <Title text={'Projects'}/>
 
                 <div className={style.projects}>
-                    <Project title={'Allthaiherbs'}
-                             description={'Lorem ipsum dolor sit amet.'}
-                             link={'https://allthaiherbs.com/ru'}
-                             linkTitle={'allthaiherbs.com'}
-                    />
-                    <Project title={'Social Network'}
-                             description={'Lorem ipsum dolor sit amet.'}
-                             linkTitle={'tagBogNet.pokaNet'}
-                             link={'https://bogagree.github.io/samurai-way-main/'}
-                    />
-                    <Project title={'Todolist'}
-                             description={'Lorem ipsum dolor sit amet.'}
-                             linkTitle={'myTodoList.pokaNet'}
-                             link={'https://bogagree.github.io/todolist-ts/'}
-                    />
+
+                    {myProjects.map(p => <Project
+                        title={p.title}
+                        description={p.description}
+                        link={p.link}
+                        linkTitle={p.linkTitle}
+                        img={p.img}
+                        technologies={p.technologies}
+                    />)}
+
                 </div>
             </div>
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,40 +1,39 @@
 import React from 'react';
 import style from './Project.module.css'
-import project_img from '../../assets/img/projectImg.jpg';
-declare module '*.jpg';
 
-
-type ProjectPropsType = {
+export type ProjectType = {
     title: string
     description: string
     img?: string
     link: string
     linkTitle: string
+    technologies: string[]
 }
 
-const Project = (props: ProjectPropsType) => {
+const Project = (props: ProjectType) => {
     return (
         <div className={style.projectCard}>
 
-            <img className={style.projectImg} src={project_img} alt="project-img"></img>
+            <img className={style.projectImg} src={props.img} alt="project-img"></img>
 
+            <div className={style.textContainer}>
 
-            <div className={style.textContainer}><h3 className={style.title}><a href="#">{props.title}</a></h3>
+                <h3 className={style.title}>
+                    <a href={props.link}>{props.title}</a>
+                </h3>
 
                 <span className={style.description}>{props.description}</span>
 
                 <p>Used stack:</p>
 
                 <ul className={style.tags}>
-                    <li>html5</li>
-                    <li>css3</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Redux</li>
+                    {props.technologies.map(t => <li>{t}</li>)}
                 </ul>
+
                 <div className={style.linkWrapper}>
                     <a className={style.link} href={props.link}>{props.linkTitle}</a>
                 </div>
+
             </div>
 
 
