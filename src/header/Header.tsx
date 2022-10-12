e13bbe4c21cc468e9d4645b6ecb86ec5dc016f6a
Bogagree <bogatireshka@gmail.com>
2022-10-12 21:07:30 +0300

    feat: add vanta.js background; refactor: header fixed

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './Header.module.scss'
 import {Nav} from "./nav/Nav";
 
 export const Header = () => {
+
+    const [position, setPosition] = useState(window.scrollY)
+    const [visible, setVisible] = useState(true)
+
+    useEffect(()=> {
+        const handleScroll = () => {
+            let moving = window.scrollY
+
+            setVisible(position > moving);
+            setPosition(moving)
+        };
+        window.addEventListener("scroll", handleScroll);
+        return(() => {
+            window.removeEventListener("scroll", handleScroll);
+        })
+    })
+
+    const cls = visible ? `${style.visible}` :  `${style.hidden}`;
+
     return (
-        <header className={style.header}>
+        <header className={`${style.header}  ${cls}`}>
             <div className={style.container}>
                 <Nav/>
             </div>
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {MainUp} from './MainUp/MainUp';
 import style from './Main.module.scss';
 import {MainDown} from './MainDown/MainDown';
+// @ts-ignore (no type declarations for vantajs)
+import TOPOLOGY from 'vanta/dist/vanta.net.min'
+import {Header} from '../header/Header';
 
 export const Main = () => {
+
+    const [vantaEffect, setVantaEffect] = useState<any>(0)
+    const myRef = useRef(null)
+
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(TOPOLOGY({
+                el: myRef.current,
+                mouseControls: true,
+                touchControls: true,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                points: 14.00,
+                maxDistance: 30.00,
+                spacing: 20.00,
+                scaleMobile: 1.00,
+                color: '#a9c96a',
+                backgroundColor: '#7766a1'
+            }))
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy()
+        }
+    }, [vantaEffect])
+
     return (
-        <section className={style.main}>
+        <section className={`${style.main}`} ref={myRef}>
             <div className={style.container}>
+                <Header/>
                 <MainUp/>
                 <MainDown/>
             </div>
+
         </section>
     )
         ;
