c93c30cf66682b56a7d67edb60e3aeb4ee0f7ab6
Bogagree <bogatireshka@gmail.com>
2022-10-12 17:18:58 +0300

    feat: add skills draft

---
+++ src/skills/Skill/Skill.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Skill.module.scss'
+
+type SkillPropsType = {
+    Icon: any
+    title: string
+}
+
+export const Skill: React.FC<SkillPropsType> = ({Icon, title}) => {
+    return (
+        <div className={style.skill}>
+            {Icon}
+            <p className={style.title}>{title}</p>
+        </div>
+    );
+};
+
+export default Skill;
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import style from './Skills.module.scss'
-import Skill from "./skill/Skill";
+import Skill from "./Skill/Skill";
 import {Title} from '../common/Components/Title/Title';
+import {SiRedux, SiTypescript} from 'react-icons/si';
+import {FaReact} from 'react-icons/fa';
+
+const skills = [
+    {icon: <SiTypescript/>, title: 'Typescript'},
+    {icon: <FaReact/>, title: 'React'},
+    {icon: <SiRedux/>, title: 'Redux'},
+]
+
+const mappedSkills = skills.map(({icon, title}) => {
+    return <Skill Icon={icon} title={title}/>
+})
 
 export const Skills = () => {
+
     return (
 
         <section>
             <div className={style.container}>
-
                 <div className={style.skillsBlock}>
-
                     <Title text={'Skills'}/>
 
-                    <div className={style.skills}>
-                        <Skill title={'HTML'}
-                               description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, possimus!'}/>
-                        <Skill title={'CSS'}
-                               description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, possimus!'}/>
-                        <Skill title={'JS'}
-                               description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, possimus!'}/>
+                    <div className={style.skillsBlock}>
+
+                        <div className={style.skills}>
+
+                            <h3>My Tech Stack</h3>
+                            <p>I've tried out lots of different technologies and am always
+                                open to new things. Here are the ones I've got most experience
+                                with:</p>
+
+                            {mappedSkills}
+
+                        </div>
                     </div>
                 </div>
-
             </div>
         </section>
-    );
-};
+    )
+}
+//
+//
+//
+//
+//
+//
+//
+//
