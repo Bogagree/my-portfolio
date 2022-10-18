630d6d557d2fd6a41722f3e2006355dc4e5db933
Bogagree <bogatireshka@gmail.com>
2022-10-18 18:54:58 +0300

    feat: add .env; add emailjs lib for Contacts.tsx (Get me in touch block)

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -5,16 +5,18 @@ import {Title} from '../common/Components/Title/Title';
 import {Footer} from '../footer/Footer';
 // @ts-ignore (no type declarations for vantajs)
 import NET from 'vanta/dist/vanta.net.min'
+import emailjs from '@emailjs/browser'
 
 export const Contacts = () => {
 
+    // background animation start
     const [vantaEffect, setVantaEffect] = useState<any>(0)
-    const myRef = useRef(null)
+    const backgroundAnimation = useRef(null)
 
     useEffect(() => {
         if (!vantaEffect) {
             setVantaEffect(NET({
-                el: myRef.current,
+                el: backgroundAnimation.current,
                 mouseControls: true,
                 touchControls: true,
                 gyroControls: false,
@@ -32,20 +34,66 @@ export const Contacts = () => {
             if (vantaEffect) vantaEffect.destroy()
         }
     }, [vantaEffect])
+    // background animation end
+
+    const form = useRef<HTMLFormElement>(null);
+
+
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault();
+
+        emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_ID || '',
+            process.env.REACT_APP_TEMPLATE_ID || '',
+            form.current || '',
+            process.env.REACT_APP_PUBLIC_KEY
+        )
+            .then((result) => {
+                console.log(result.text)
+                alert('Your message has been sent successfully!')
+            }, (error) => {
+                console.log(error.text);
+            });
+        form.current && form.current.reset()
+
+    };
 
     return (
-        <section id={'contacts'} className={style.getInTouch} ref={myRef}>
+        <section id={'contacts'} className={style.getInTouch} ref={backgroundAnimation}>
 
             <div className={style.container}>
 
                 <Title text={'Get in touch'} color={'white'}/>
 
-                <form className={style.form}>
+                <form className={style.form} ref={form} onSubmit={sendEmail}>
                     <div className={style.formWrapper}>
-                        <input className={style.input} type="text" placeholder="Your name"></input>
-                        <input className={style.input} type="text" placeholder="Your email"></input>
-                        <textarea className={style.textarea} placeholder="Type the message here"></textarea>
-                        <SiteButton className={style.sendBtn}>send</SiteButton>
+
+                        <label htmlFor="name">Name:</label>
+                        <input className={style.input}
+                               type="text"
+                               placeholder="Your name"
+                               name="user_name"
+                               required
+                        />
+
+                        <label htmlFor="email">Email:</label>
+                        <input className={style.input}
+                               type="text"
+                               placeholder="Your email"
+                               name="user_email"
+                               required
+                        />
+
+                        <label htmlFor="message">Message:</label>
+                        <textarea className={style.textarea}
+                                  placeholder="Type the message here"
+                                  name="message"
+                        />
+
+                        <SiteButton className={style.sendBtn} type="submit">send</SiteButton>
+
                     </div>
                 </form>
 
